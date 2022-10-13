c42acf33c2a2108125b2cbb76c603ef98188eb4b
PeeperInTheSpace <lollof94@gmail.com>
2022-10-13 20:15:30 +0200

    Snack 5: Completed!

--- SNACK-5/index.php
+++ SNACK-5/index.php
@@ -11,6 +11,60 @@
         <!-- Creare un array contenente qualche alunno di un’ipotetica classe. 
         Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. 
         Stampare Nome, Cognome e la media dei voti di ogni alunno. -->
+
+        <?php
+
+            $class = [
+
+                    [
+                        'name' => 'John',
+                        'surname' => 'Mlem',
+                        'votes' => [10, 9, 5, 5, 6]
+                    ],
+                    [
+                        'name' => 'Clara',
+                        'surname' => 'Sbriz',
+                        'votes' => [9, 8, 3, 10, 5]
+                    ],
+
+                    [
+                        'name' => 'George',
+                        'surname' => 'Flex',
+                        'votes' => 'Testo post 3'
+                    ],
+
+                    [
+                        'name' => 'Jessica',
+                        'surname' => 'Plum',
+                        'votes' => [10, 9, 8, 10, 5]
+                    ],
+                    [
+                        'name' => 'Bruno',
+                        'surname' => 'Jupiter',
+                        'votes' => [2, 9, 8, 4, 5]
+                    ],
+                    [
+                        'name' => 'Kelly',
+                        'surname' => 'Clarkson',
+                        'votes' => [10, 4, 8, 6, 5]
+                    ]
+                ];
+
+                for ($i = 0; $i < count($class); $i++) {
+
+                    $mediaVoti = 0;
+                
+                    for ($j = 0; $j < count($class[$i]["voti"]); $j++) {
+                        $mediaVoti = $mediaVoti + $class[$i]["voti"][$j];
+                    }
+            
+                    echo "<strong style='font-family: sans-serif;'>" . $class[$i]["name"] . " " . "</strong>";
+                    echo "<strong style='font-family: sans-serif;'>" . $class[$i]["surname"] . "</strong><br>";    
+                    echo $mediaVoti / (count($class[$i]["voti"])) . "<br>";
+                
+                }
+                
+        ?>
     </body>
     
 </html>
