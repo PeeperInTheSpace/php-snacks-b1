94111c306314f75981e87f1c566d743d679e2c94
PeeperInTheSpace <lollof94@gmail.com>
2022-10-13 19:01:09 +0200

    Snack 2: Completed!

--- SNACK-2/index.php
+++ SNACK-2/index.php
@@ -11,6 +11,38 @@
         <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
         che mail contenga un punto e una chiocciola e che age sia un numero. 
         Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+        <?php
+
+            $name = $_GET['name'] ?? '';
+            $age = $_GET['age'] ?? '';
+            $email = $_GET['email'] ?? '';
+
+            $name_verify = false;
+            $email_verify = false;
+            $age_verify = false;
+
+            if (isset($_GET['name']) && strlen($name) > 3) {
+                $name_verify = true;
+            }
+
+            if (isset($_GET['email']) && strpos($email, '.') && strpos($email, '@')) {
+                $email_verify = true;
+            }
+            if (isset($_GET['age']) && is_numeric($age)) {
+
+                $age_verify = true;
+            }
+
+            if ($name_verify === true && $email_verify === true && $age_verify === true) {
+                echo "<h1 style='color: green; font-family: sans-serif;'> Accesso consentito! </h1>";
+            } else {
+                echo "<h1 style='color: red; font-family: sans-serif;'> Accesso negato! </h1>";
+            }
+
+        ?>
+
+
     </body>
     
 </html>
