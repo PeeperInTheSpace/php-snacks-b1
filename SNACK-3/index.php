2c15300f3bd2fd242b852b2c7b389dcbb180f4fa
PeeperInTheSpace <lollof94@gmail.com>
2022-10-13 18:26:43 +0200

    Snack 3 & 4: Completed!

--- SNACK-3/index.php
+++ SNACK-3/index.php
@@ -11,6 +11,64 @@
         <!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: 
         DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
         Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+
+        <?php
+
+            $posts = [
+
+                '10/01/2019' => [
+                    [
+                        'title' => 'Post 1',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 1'
+                    ],
+                    [
+                        'title' => 'Post 2',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 2'
+                    ],
+                ],
+
+                '10/02/2019' => [
+                    [
+                        'title' => 'Post 3',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 3'
+                    ]
+                ],
+
+                '15/05/2019' => [
+                    [
+                        'title' => 'Post 4',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 4'
+                    ],
+                    [
+                        'title' => 'Post 5',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 5'
+                    ],
+                    [
+                        'title' => 'Post 6',
+                        'author' => 'Michele Papagni',
+                        'text' => 'Testo post 6'
+                    ]
+                ],
+            ];
+
+            $keys = array_keys($posts);
+            for ($i = 0; $i < count($posts); $i++) {
+
+                echo "<h1>" . $keys[$i] . "</h1>";
+        
+                    foreach ($posts[$keys[$i]] as $key => $value) {
+                        echo "<h3>" . $value['author'] . " : " . $value['title'] . " : " . $value['text'] . "</h3>";
+                    }
+
+                    echo "<br>";
+                }
+        ?>
+
     </body>
-    
+
 </html>
--- SNACK-4/index.php
+++ SNACK-4/index.php
@@ -11,6 +11,21 @@
         <!-- Prendere un paragrafo abbastanza lungo, contenente diverse frasi. 
         Prendere il paragrafo e suddividerlo in tanti paragrafi. 
         Ogni punto un nuovo paragrafo. -->
+
+        <?php 
+
+        $paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius maxime, pariatur tempora aspernatur minima quam impedit nulla alias maiores amet autem error doloribus minus, quos veritatis suscipit omnis veniam animi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet voluptate quia provident est excepturi earum explicabo dicta aliquam iure, praesentium laboriosam eum distinctio aliquid vero. Perferendis rerum debitis quam architecto? Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quas ipsam, reprehenderit nihil numquam nam laborum alias modi earum maiores at beatae dicta enim perspiciatis. Molestiae sunt necessitatibus voluptatibus eius?";
+
+        $explodingParagraph = explode(".", $paragraph);
+
+        echo "<h1>" . $paragraph . "</h1>";
+
+        foreach ($explodingParagraph as $newParagraph) {
+            echo "<br>" . $newParagraph . "<br>";
+        }
+
+        ?>
+
     </body>
     
 </html>
